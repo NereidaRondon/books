1a43983c9320da99c2ca7d2e9bf871dc6b4ff981
nereidarondon <nereida.rondon5@gmail.com>
2026-05-13 13:01:55 -0400

    added gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
 import express from "express"
 import mysql from "mysql2"
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 // Create an instance of the Express application
 const app = express()
 
 // Create a connection to the MySQL database
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "database@1984",
-  database: "books_crud_app"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 })
 
 // Middleware to parse JSON bodies
